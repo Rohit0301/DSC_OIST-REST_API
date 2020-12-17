98863a5c3018840203ffbaf399b8f2369f5d6c82
Rohit0301 <rj03012002@gmail.com>
2020-12-18 00:17:09 +0530

    Listapi view created

--- Team/views.py
+++ Team/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
+from .models import TeamMembers 
+from .serializers import TeamMembersSerializer
+from rest_framework import permissions
 # Create your views here.
+
+class ContactListView(ListAPIView):
+    serializer_class=TeamMembersSerializer
+
+    def get_queryset(self):
+        return TeamMembers.objects.get()
+
+# class ContactDetailView(RetrieveUpdateDestroyAPIView):
+#     serializer_class=TeamMembersSerializer
+#     permission_classes=(permissions.IsAuthenticated,)
+#     lookup_field="id"
+        
+#     def get_queryset(self):
+#         return Contact.objects.filter(owner=self.request.user)
