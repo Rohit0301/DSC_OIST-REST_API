f1a6d7cbac4eaeba5b3ebb541d0cecb253045281
Rohit0301 <rj03012002@gmail.com>
2020-12-18 23:27:59 +0530

    changed the classes names

--- Events/views.py
+++ Events/views.py
@@ -5,20 +5,20 @@ from .serializers import EventsSerializer
 from rest_framework import permissions
 # Create your views here.
 
-class TeamMemberListView(ListAPIView):
+class EventListView(ListAPIView):
     serializer_class=EventsSerializer
 
     def get_queryset(self):
         return Event.objects.all()
 
-class TeamMemberCreateView(CreateAPIView):
+class EventCreateView(CreateAPIView):
     serializer_class=EventsSerializer
     permission_classes=(permissions.IsAdminUser,)
     def perform_create(self,serializer):
         serializer.save()
 
 
-class TeamMemberDetailView(RetrieveUpdateDestroyAPIView):
+class EventDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class=EventsSerializer
     # permission_classes=(permissions.IsAuthenticated,)
     lookup_field="id"
