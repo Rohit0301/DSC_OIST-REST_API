ec794cf3d91a3fb5b733ce0fe729b97b6f4181f4
Rohit0301 <rj03012002@gmail.com>
2020-12-18 23:50:16 +0530

    include resources url

--- DSC_OIST/urls.py
+++ DSC_OIST/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('Team.urls')),
     path('api/',include('Events.urls')),
+    path('api/',include('Resources.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
