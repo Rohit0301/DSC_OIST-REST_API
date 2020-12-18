a8ba770d6e7e361fb3314c68b40d3731cc288f3b
Rohit0301 <rj03012002@gmail.com>
2020-12-18 23:22:54 +0530

    Event serializer added

---
+++ Events/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Event
+class EventsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Event
+        fields=['id','title','subtitle','description','speaker','date','time','event_link']
