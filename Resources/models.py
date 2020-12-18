3d60a85ba68de8b4a40f1716a302d092a7fcb7bf
Rohit0301 <rj03012002@gmail.com>
2020-12-18 23:41:49 +0530

    resources model created

--- Resources/models.py
+++ Resources/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Resource(models.Model):
+    name=models.CharField(max_length=50)
+    domain=models.CharField(max_length=20)
+    resource_url=models.URLField(null=True)
+    resource_file=models.FileField(upload_to='static/resources/',null=True)
+
+    def __str__(self):
+        return self.name
