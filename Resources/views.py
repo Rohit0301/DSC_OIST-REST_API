5527c7d21ea4128adafe4760fe83445d7231fb66
Rohit0301 <rj03012002@gmail.com>
2020-12-18 23:45:31 +0530

    resource view are created

--- Resources/views.py
+++ Resources/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
+from .models import Resource
+from .serializers import ResourcesSerializer
+from rest_framework import permissions
 # Create your views here.
+
+class ResourseListView(ListAPIView):
+    serializer_class=ResourcesSerializer
+
+    def get_queryset(self):
+        return Resource.objects.all()
+
+class ResourceCreateView(CreateAPIView):
+    serializer_class=ResourcesSerializer
+    permission_classes=(permissions.IsAdminUser,)
+    def perform_create(self,serializer):
+        serializer.save()
+
+
+class ResourceDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class=ResourcesSerializer
+    # permission_classes=(permissions.IsAuthenticated,)
+    lookup_field="id"
+        
+    def get_queryset(self):
+        return Resource.objects.filter(id=self.kwargs['id'])
