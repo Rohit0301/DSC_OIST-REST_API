8e93f7cab5d5e40f0821bc3ca79a6b348d389fa1
Rohit0301 <rj03012002@gmail.com>
2020-12-18 23:24:34 +0530

    view are added in event app

--- Events/views.py
+++ Events/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
+from .models import Event
+from .serializers import EventsSerializer
+from rest_framework import permissions
 # Create your views here.
+
+class TeamMemberListView(ListAPIView):
+    serializer_class=EventsSerializer
+
+    def get_queryset(self):
+        return Event.objects.all()
+
+class TeamMemberCreateView(CreateAPIView):
+    serializer_class=EventsSerializer
+    permission_classes=(permissions.IsAdminUser,)
+    def perform_create(self,serializer):
+        serializer.save()
+
+
+class TeamMemberDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class=EventsSerializer
+    # permission_classes=(permissions.IsAuthenticated,)
+    lookup_field="id"
+        
+    def get_queryset(self):
+        return Event.objects.filter(id=self.kwargs['id'])
