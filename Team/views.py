3e41cb80a03d7c925486bc927f7ee577c5023cd5
Rohit0301 <rj03012002@gmail.com>
2020-12-21 00:02:07 +0530

    added auth and  permissions

--- Team/views.py
+++ Team/views.py
@@ -3,25 +3,28 @@ from django.http import HttpResponse
 from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
 from .models import TeamMember
 from .serializers import TeamMembersSerializer
-from rest_framework import permissions
+from rest_framework.permissions import IsAdminUser,BasePermission,SAFE_METHODS,IsAuthenticated,AllowAny
+from rest_framework.authentication import BasicAuthentication
 # Create your views here.
 
 class TeamMemberListView(ListAPIView):
     serializer_class=TeamMembersSerializer
-
+    permission_classes=[AllowAny]
     def get_queryset(self):
         return TeamMember.objects.all()
 
 class TeamMemberCreateView(CreateAPIView):
     serializer_class=TeamMembersSerializer
-    permission_classes=(permissions.IsAdminUser,)
+    authentication_classes=[BasicAuthentication]
+    permission_classes=[IsAdminUser]
     def perform_create(self,serializer):
         serializer.save()
 
 
 class TeamMemberDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class=TeamMembersSerializer
-    # permission_classes=(permissions.IsAuthenticated,)
+    authentication_classes=[BasicAuthentication]
+    permission_classes=[IsAdminUser]
     lookup_field="fullname"
         
     def get_queryset(self):
