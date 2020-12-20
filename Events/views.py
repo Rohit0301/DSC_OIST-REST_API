a27b155c5cbd1d39d0ac901c2adc881aa3509517
Rohit0301 <rj03012002@gmail.com>
2020-12-21 00:01:55 +0530

    added auth and  permissions

--- Events/views.py
+++ Events/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
 from .models import Event
+from rest_framework.authentication import BasicAuthentication
 from .serializers import EventsSerializer
-from rest_framework import permissions
+from rest_framework.permissions import IsAdminUser,AllowAny
 # Create your views here.
 
 class EventListView(ListAPIView):
     serializer_class=EventsSerializer
-
+    permission_classes=[AllowAny]
     def get_queryset(self):
         return Event.objects.all()
 
 class EventCreateView(CreateAPIView):
     serializer_class=EventsSerializer
-    permission_classes=(permissions.IsAdminUser,)
+    # authentication_classes=[BasicAuthentication]
+    # permission_classes=[IsAdminUser]
     def perform_create(self,serializer):
         serializer.save()
 
 
 class EventDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class=EventsSerializer
-    # permission_classes=(permissions.IsAuthenticated,)
+    # authentication_classes=[BasicAuthentication]
+    # permission_classes=[IsAdminUser]
     lookup_field="title"
         
     def get_queryset(self):
