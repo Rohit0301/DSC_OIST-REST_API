46fa1fa646f8b1cd7e472838d2c37e3428561572
Rohit0301 <rj03012002@gmail.com>
2020-12-21 00:01:46 +0530

    added auth and  permissions

--- Resources/views.py
+++ Resources/views.py
@@ -2,25 +2,28 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
 from .models import Resource
 from .serializers import ResourcesSerializer
-from rest_framework import permissions
+from rest_framework.permissions import IsAdminUser,AllowAny
+from rest_framework.authentication import BasicAuthentication
 # Create your views here.
 
 class ResourceListView(ListAPIView):
     serializer_class=ResourcesSerializer
-
+    permission_classes=[AllowAny]
     def get_queryset(self):
         return Resource.objects.all()
 
 class ResourceCreateView(CreateAPIView):
     serializer_class=ResourcesSerializer
-    permission_classes=(permissions.IsAdminUser,)
+    # authentication_classes=[BasicAuthentication]
+    # permission_classes=[IsAdminUser]
     def perform_create(self,serializer):
         serializer.save()
 
 
 class ResourceDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class=ResourcesSerializer
-    # permission_classes=(permissions.IsAuthenticated,)
+    # authentication_classes=[BasicAuthentication]
+    # permission_classes=[IsAdminUser]
     lookup_field="name"
         
     def get_queryset(self):
