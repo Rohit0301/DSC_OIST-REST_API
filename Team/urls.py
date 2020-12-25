4e6e606c93b7bfe12d767d1fc5c6a6322fadec95
Rohit0301 <rj03012002@gmail.com>
2020-12-25 14:50:12 +0530

    deleted the create api url and view

--- Team/urls.py
+++ Team/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import TeamMemberListView,TeamMemberCreateView,TeamMemberDetailView
+from .views import TeamMemberListView,TeamMemberDetailView
 urlpatterns = [
   path('TeamMembers/',TeamMemberListView.as_view()),
-  path('AddMember/',TeamMemberCreateView.as_view()),
-  path('MemberDetails/<fullname>/',TeamMemberDetailView.as_view()),
+  # path('AddMember/',TeamMemberCreateView.as_view()),
+  path('TeamMembers/<fullname>/',TeamMemberDetailView.as_view()),
 ]
--- Team/views.py
+++ Team/views.py
@@ -1,32 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
 from .models import TeamMember
 from .serializers import TeamMembersSerializer
-from rest_framework.permissions import IsAdminUser,BasePermission,SAFE_METHODS,IsAuthenticated,AllowAny
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAdminUser,BasePermission,SAFE_METHODS,IsAuthenticatedOrReadOnly
+from rest_framework.authentication import SessionAuthentication
 # Create your views here.
+class IsAdminUserOrReadOnly(IsAdminUser):
 
-class TeamMemberListView(ListAPIView):
+    def has_permission(self, request, view):
+        is_admin = super(
+            IsAdminUserOrReadOnly, 
+            self).has_permission(request, view)
+        # Python3: is_admin = super().has_permission(request, view)
+        return request.method in SAFE_METHODS or is_admin
+
+class TeamMemberListView(ListCreateAPIView):
     serializer_class=TeamMembersSerializer
-    permission_classes=[AllowAny]
+    permission_classes=[IsAdminUserOrReadOnly]
     def get_queryset(self):
         return TeamMember.objects.all()
-
-class TeamMemberCreateView(CreateAPIView):
-    serializer_class=TeamMembersSerializer
-    authentication_classes=[BasicAuthentication]
-    permission_classes=[IsAdminUser]
     def perform_create(self,serializer):
         serializer.save()
+
+# class TeamMemberCreateView(CreateAPIView):
+#     serializer_class=TeamMembersSerializer
+#     authentication_classes=[SessionAuthentication]
+#     permission_classes=[IsAdminUser]
+#     def perform_create(self,serializer):
+#         serializer.save()
 
 
 class TeamMemberDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class=TeamMembersSerializer
-    authentication_classes=[BasicAuthentication]
-    permission_classes=[IsAdminUser]
+    authentication_classes=[SessionAuthentication]
+    permission_classes=[IsAdminUserOrReadOnly]
     lookup_field="fullname"
         
     def get_queryset(self):
-        
         return TeamMember.objects.filter(fullname__contains=self.kwargs['fullname'])
