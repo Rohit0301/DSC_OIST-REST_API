4706fa14490f62ac87cfcd4ae7f340e7c0f769cd
Rohit0301 <rj03012002@gmail.com>
2020-12-25 14:49:39 +0530

    delete create api urls and view

--- Events/urls.py
+++ Events/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import EventListView,EventCreateView,EventDetailView
+from .views import EventListView,EventDetailView
 urlpatterns = [
-  path('AllEvents/',EventListView.as_view()),
-  path('AddNewEvent/',EventCreateView.as_view()),
+  path('Event/',EventListView.as_view()),
+  # path('AddNewEvent/',EventCreateView.as_view()),
   path('Event/<title>/',EventDetailView.as_view()),
 ]
--- Events/views.py
+++ Events/views.py
@@ -1,29 +1,39 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
 from .models import Event
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.authentication import BasicAuthentication,SessionAuthentication
 from .serializers import EventsSerializer
-from rest_framework.permissions import IsAdminUser,AllowAny
+from rest_framework.permissions import IsAdminUser,AllowAny,SAFE_METHODS
 # Create your views here.
+class IsAdminUserOrReadOnly(IsAdminUser):
 
-class EventListView(ListAPIView):
+    def has_permission(self, request, view):
+        is_admin = super(
+            IsAdminUserOrReadOnly, 
+            self).has_permission(request, view)
+        # Python3: is_admin = super().has_permission(request, view)
+        return request.method in SAFE_METHODS or is_admin
+
+class EventListView(ListCreateAPIView):
     serializer_class=EventsSerializer
-    permission_classes=[AllowAny]
+    permission_classes=[IsAdminUserOrReadOnly]
     def get_queryset(self):
         return Event.objects.all()
-
-class EventCreateView(CreateAPIView):
-    serializer_class=EventsSerializer
-    # authentication_classes=[BasicAuthentication]
-    # permission_classes=[IsAdminUser]
     def perform_create(self,serializer):
         serializer.save()
+
+# class EventCreateView(CreateAPIView):
+#     serializer_class=EventsSerializer
+#     authentication_classes=[SessionAuthentication]
+#     permission_classes=[IsAdminUser]
+#     def perform_create(self,serializer):
+#         serializer.save()
 
 
 class EventDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class=EventsSerializer
-    # authentication_classes=[BasicAuthentication]
-    # permission_classes=[IsAdminUser]
+    authentication_classes=[SessionAuthentication]
+    permission_classes=[IsAdminUserOrReadOnly]
     lookup_field="title"
         
     def get_queryset(self):
